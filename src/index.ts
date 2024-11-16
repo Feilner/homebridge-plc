77d11921c28e171e791d673bd81f41feea079111
Feilner <feilner@your-mail.com>
2024-11-16 16:28:02 +0100

    import homebridge-plugin-template

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import type { API } from 'homebridge';
+
+import { ExampleHomebridgePlatform } from './platform.js';
+import { PLATFORM_NAME } from './settings.js';
+
+/**
+ * This method registers the platform with Homebridge
+ */
+export default (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+};
